47c21c9fdf7e49f01a2e43379ec4075952771ca7
AcoverI <70806873+Unitysourceman@users.noreply.github.com>
2023-12-21 22:34:13 +0300

    START.BAT

---
+++ start.bat
@@ -0,0 +1,11 @@
+@echo WAIT!
+pip install aiohttp==3.8.6
+pip install pendulum==2.1.2
+pip install Pillow==10.0.1
+pip install python-dotenv==1.0.0
+pip install vkbottle==4.3.12
+
+@echo EVERYTHING IS Ok!
+
+python main.py
+
