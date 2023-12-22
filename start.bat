3aa52b7cd53cdfb1fcea1ec25a5e163ebf916759
cursed <70806873+Unitysourceman@users.noreply.github.com>
2023-12-22 10:26:34 +0300

    nemnogo pogovnokodil

--- start.bat
+++ start.bat
@@ -1,11 +1,26 @@
-@echo WAIT!
+@echo Ожидайте!
+
+@echo Загружаю aiohttp!
 pip install aiohttp==3.8.6
+
+@echo Загружаю pendulum!
 pip install pendulum==2.1.2
+
+@echo Загружаю pillow!
 pip install Pillow==10.0.1
+
+@echo Загружаю dotenv!
 pip install python-dotenv==1.0.0
+
+@echo Загружаю vkbottle!
 pip install vkbottle==4.3.12
 
-@echo EVERYTHING IS Ok!
+@echo Загружаю код!
 
 python main.py
 
+sleep 30
+
+@echo НАСЛАЖДАЙТЕСЬ!
+
+rem ну и говнокод...
