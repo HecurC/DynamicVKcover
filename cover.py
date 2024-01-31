307c6a07020fbf9deb8cd299aad767d48136b550
Cursed <70806873+Unitysourceman@users.noreply.github.com>
2024-01-31 19:34:13 +0300

    mama tvoya

--- cover.py
+++ cover.py
@@ -1,5 +1,4 @@
 import logging
-
 from io import BytesIO
 from aiohttp import FormData, ClientSession
 
@@ -13,7 +12,7 @@ class CoverImage:
     WIDTH, HEIGHT = 1920, 768
     FILL = "#FFFFFF"
     STROKE_FILL = "#000000"
-    FONT = " Cruinn.ttf"
+    FONT = "Cruinn.ttf"
 
     def __init__(self, api: API, user_id: int) -> None:
         self._api = api
@@ -21,8 +20,7 @@ class CoverImage:
 
         self.buffer = None
 
-    async def get_random_image(self) -> BytesIO:
-        url = "https://minimalistic-wallpaper.demolab.com?random"
+    async def get_random_image(self, url) -> BytesIO:
         image_buffer = BytesIO()
 
         async with ClientSession() as session:
@@ -30,10 +28,10 @@ class CoverImage:
 
             async for chunk in response.content.iter_chunked(1024):
                 image_buffer.write(chunk)
-            return image_buffer
+        return image_buffer
 
-    async def draw_cover(self) -> None:
-        random_image = await self.get_random_image()
+    async def draw_cover(self, url) -> None:
+        random_image = await self.get_random_image(url=url)
 
         img = Image.open(BytesIO(random_image.getvalue()))
         img = img.convert("RGBA").resize((self.WIDTH, self.HEIGHT))
@@ -47,8 +45,6 @@ class CoverImage:
         current_time = current_datetime.strftime("%H:%M")
         formatted_date = current_datetime.to_formatted_date_string()
 
-        # А здесь вы можете добавить новый текст на обложку
-
         draw.text(
             (self.WIDTH / 2, self.HEIGHT / 2), current_time, self.FILL, font_time, "ms", stroke_width=3,
             stroke_fill=self.STROKE_FILL
@@ -60,6 +56,16 @@ class CoverImage:
         draw.text(
             (self.WIDTH / 2 + 700, self.HEIGHT / 2 + 200), "Обложка меняется каждые 10 минут", self.FILL, font_alert,
             "ms", stroke_width=2,
+            stroke_fill=self.STROKE_FILL
+        )
+        draw.text(
+            (self.WIDTH / 2 + 700, self.HEIGHT / 2 + 170), "@bogdanihoor4", self.FILL,
+            font_alert, "ms", stroke_width=2,
+            stroke_fill=self.STROKE_FILL
+        )
+        draw.text(
+            (self.WIDTH / 2 + 700, self.HEIGHT / 2 + 370), "Привет, человек с телефона!", self.FILL,
+            font_alert, "ms", stroke_width=2,
             stroke_fill=self.STROKE_FILL
         )
 
